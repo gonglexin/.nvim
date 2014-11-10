8d87b0f585dfdcdbc6d27be932b6e206da187ac2
Lexin Gong <gonglexin@gmail.com>
2014-11-10 21:44:11 +0800

    A one-step install script

--- install.sh
+++ install.sh
@@ -1,12 +1,23 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 echo ""
-echo "Deploy nvimrc ......"
 
-rm -f $HOME/.nvimrc
+NVIM_HOME=~/.nvim
+BUNDLE_DIR=$NVIM_HOME/bundle
 
-cd $HOME/.nvim
-mkdir bundle
-cd bundle
-git clone https://github.com/Shougo/neobundle.vim
+if [ -e $NVIM_HOME ]; then
+  echo "$NVIM_HOME already exists!"
+  exit 1
+fi
+
+echo "Fetching nvimrc..."
+rm -rf "$NVIM_HOME"
+git clone git@github.com:gonglexin/.nvim.git $NVIM_HOME
+
+echo "Fetching NeoBundle..."
+mkdir -p $NVIM_HOME/bundle
+git clone https://github.com/Shougo/neobundle.vim $NVIM_HOME/bundle/neobundle.vim
+
+echo "Installing plugins..."
+nvim +NeoBundleInstall +q
 
 echo "Done."
